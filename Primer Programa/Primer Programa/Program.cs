dbe2212320d5787e17165337b76396881333d2bc
CrisGold22 <cristhianhoracio22@gmail.com>
2025-04-22 00:48:37 -0500

    update

--- Primer Programa/Primer Programa/Program.cs
+++ Primer Programa/Primer Programa/Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("Pacheco se la come entera");
+﻿using System;
+using System.Transactions;
+
+namespace PrimerPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Aqui se indican los diferentes tipos de variables
+            int numeroEntero = 10; // Variable de tipo entero
+            double numeroDecimal = 20.5; // Variable de tipo decimal
+            string texto = "Hola, mundo!"; // Variable de tipo cadena
+            bool esVerdadero = true; // Variable de tipo booleano
+            char letra = 'A'; // Variable de tipo caracter
+
+            // Concatenacion de cadenas
+            texto += " Mi nombre es CrisGold";
+            Console.WriteLine(texto);
+
+            // Ejemplo basico de casteo de un string a un entero
+            string numeroCadena = "123";
+            int numero = int.Parse(numeroCadena);  // La funcion Parse() convierte un string a un entero
+            Console.WriteLine("El numero es: " + numero);
+
+            //Ahora con el caso inverso
+            int numeroEntero2 = 456;
+            numeroCadena = numeroEntero2.ToString(); // La funcion ToString() convierte un entero a un string
+            Console.WriteLine("El numero es: " + numeroCadena);
+
+        }
+    }
+}
--- Primer Programa/Primer Programa/obj/Debug/net8.0/Primer Programa.AssemblyInfo.cs
+++ Primer Programa/Primer Programa/obj/Debug/net8.0/Primer Programa.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Primer Programa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1679d883f0599e93125459dc94dbdf555d37821")]
 [assembly: System.Reflection.AssemblyProductAttribute("Primer Programa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Primer Programa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
